5e1e9ba109469f483e2ae1ef1a251ff1d99368f1
Steven Luu <Stevenluuchs@gmail.com>
2023-11-08 15:42:22 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Steven Luu on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
